3cef259ba05d5e679d81ff882805022f96b7512d
Tazul Islam <linkedin52425@gmail.com>
2026-08-04 09:09:19 +0600

    change photo

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import myImage from "../assets/tazul.png";
+import myImage from "../assets/tazul.jpg";
 import myResume from "../assets/Tazul_Islam_resume.pdf";
 
 const Hero = () => {
